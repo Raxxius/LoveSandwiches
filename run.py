fb8fb83ad1a386f8daae837e6a71db41a102c43f
Raxxius <Raxxman@gmail.com>
2021-11-20 12:09:18 +0000

    add update surplus worksheet function

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
+""" from pprint import pprint """
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,6 +12,7 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
 
 def get_sales_data():
     """
@@ -45,15 +46,18 @@ def validate_data(values):
     print(values)
     try:
         [int(value) for value in values]
-        if len(values) !=6:
+
+        if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
+
     except ValueError as e:
         print(f"invalid data: {e}. Please try again.")
         return False
 
     return True
+
 
 def update_sales_worksheet(data):
     """
@@ -64,6 +68,16 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated succesfully.\n")
 
+
+def update_surplus_worksheet(new_surplus_data):
+    """
+    Update the surplus worksheet, add new row with the data calculated
+    from calculate_surplus_data
+    """
+    print("updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(new_surplus_data)
+    print("Update complete, surplus added to GoogleSheets")
 
 def calculate_surplus_data(sales_row):
     """
@@ -77,10 +91,10 @@ def calculate_surplus_data(sales_row):
     stock_row = stock[-1]
 
     surplus_data = []
-    for stock, sales in zip(stock_row, sales_row): 
+    for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - int(sales)
         surplus_data.append(surplus)
-    
+ 
     return surplus_data
 
 def main():
@@ -91,6 +105,7 @@ def main():
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
     print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
